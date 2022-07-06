97e10457a711ffa28b93c102a6ed244c56af24df
Julien Forner <jforner@c2r5p8.42nice.fr>
2022-07-06 18:13:21 +0200

    cpp1-2

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jforner <jforner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 18:24:28 by jforner           #+#    #+#             */
-/*   Updated: 2022/07/06 15:04:49 by jforner          ###   ########.fr       */
+/*   Updated: 2022/07/06 18:12:31 by jforner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,8 @@ int	main(void)
 	zomboss = zombieHorde(nbr ,"Crazy Dave");
 	for (int i = 0; i < nbr; i++)
 		zomboss[i].announce();
-	for (int i = 0; i < n; i++)
-		zomb[i].~Zombie();
-	for (int i = 0; i < nbr; i++)
-		zomboss[i].~Zombie();
+	delete [] zomb;
+	delete [] zomboss;
 	// system("leaks horde");
 	return (0);
 }
